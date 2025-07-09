b2b625ca048d008d0c2fe0460f186b962c4abc0f
laxyshkaaa <parvadov.pavel@gmail.com>
2025-07-09 19:50:48 +0300

    added: app configuration

---
+++ book-service/internal/app/app.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	grpcapp "github.com/PavelParvadov/grpc_order_book_service/book-service/internal/app/grpc"
+	"github.com/PavelParvadov/grpc_order_book_service/book-service/internal/config"
+	"github.com/PavelParvadov/grpc_order_book_service/book-service/internal/repository/postgres"
+	"github.com/PavelParvadov/grpc_order_book_service/book-service/internal/service"
+	"go.uber.org/zap"
+)
+
+type App struct {
+	BookGrpc *grpcapp.BookGrpcApp
+}
+
+func NewApp(log *zap.Logger, cfg config.Config) *App {
+	storage := postgres.NewStorage(&cfg)
+	BookService := service.NewBookService(log, storage, storage)
+	GrpcServer := grpcapp.NewApp(log, cfg.GRPCConf.Port, BookService)
+	return &App{
+		BookGrpc: GrpcServer,
+	}
+}
---
+++ book-service/internal/app/grpc/app.go
@@ -0,0 +1,51 @@
+package grpc
+
+import (
+	"fmt"
+	GrpcServer "github.com/PavelParvadov/grpc_order_book_service/book-service/internal/delivery/grpc"
+	"github.com/PavelParvadov/grpc_order_book_service/book-service/internal/service"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type BookGrpcApp struct {
+	Log  *zap.Logger
+	Grpc *grpc.Server
+	Port int
+}
+
+func NewApp(log *zap.Logger, port int, bookService *service.BookService) *BookGrpcApp {
+	server := grpc.NewServer()
+	GrpcServer.RegisterGRPCServer(server, bookService)
+	return &BookGrpcApp{
+		Log:  log,
+		Grpc: server,
+		Port: port,
+	}
+
+}
+
+func (a *BookGrpcApp) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (a *BookGrpcApp) Run() error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", a.Port))
+	if err != nil {
+		return err
+	}
+	a.Log.Info("grpc server listening on", zap.Int("port", a.Port))
+
+	if err = a.Grpc.Serve(lis); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *BookGrpcApp) Stop() {
+	a.Log.Info("grpc server stopping")
+	a.Grpc.GracefulStop()
+}
